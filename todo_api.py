e82a59a8c63ec8f7db4b49d9a94271d4bec8ce63
JFesting <julfesting@gmail.com>
2024-08-25 16:52:15 +0100

    Remove venv and __pycache__ from tracking

--- todo_api.py
+++ todo_api.py
@@ -1,8 +1,16 @@
+from databases import Database
 from quart import Quart
 
 app = Quart(__name__)
+app.config.update({
+    "DATABASE_URL": "postgresql://todo:@localhost:5432/todo"
+})
 
 @app.route("/")
 async def index() -> str:
     return "hi"
 
+async def _create_db_connection() -> Database:
+    db = Database(app.config["DATABASE_URL"])
+    await db.connect()
+    return db
